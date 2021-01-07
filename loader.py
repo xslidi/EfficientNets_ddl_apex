0f350b19cb5f6949cbc9c0b17ed00864a961c561
xslidi <xslidi@gmail.com>
2021-01-07 22:51:35 +0800

    prefetch test (failed)

--- loader.py
+++ loader.py
@@ -1,8 +1,10 @@
 import torch
+import numpy as np
 
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
 from torchvision import transforms
+from utils import ToNumpy
 # from base import DALIDataloader
 # from imagenet import HybridTrainPipe, HybridValPipe
 
@@ -10,43 +12,164 @@ IMAGENET_IMAGES_NUM_TRAIN = 1281166
 # IMAGENET_IMAGES_NUM_TRAIN = 50000
 IMAGENET_IMAGES_NUM_TEST = 50000
 VAL_SIZE = 256
+IMAGENET_DEFAULT_MEAN = (0.485, 0.456, 0.406)
+IMAGENET_DEFAULT_STD = (0.229, 0.224, 0.225) 
 
-def get_loaders(root, batch_size, resolution, num_workers=32, val_batch_size=200):
+
+class PrefetchLoader:
+
+    def __init__(self,
+                 loader,
+                 mean=IMAGENET_DEFAULT_MEAN,
+                 std=IMAGENET_DEFAULT_STD,
+                 fp16=False,
+                 re_prob=0.,
+                 re_mode='const',
+                 re_count=1,
+                 re_num_splits=0):
+        self.loader = loader
+        self.mean = torch.tensor([x * 255 for x in mean]).cuda().view(1, 3, 1, 1)
+        self.std = torch.tensor([x * 255 for x in std]).cuda().view(1, 3, 1, 1)
+        self.fp16 = fp16
+        if fp16:
+            self.mean = self.mean.half()
+            self.std = self.std.half()
+        # if re_prob > 0.:
+        #     self.random_erasing = RandomErasing(
+        #         probability=re_prob, mode=re_mode, max_count=re_count, num_splits=re_num_splits)
+        # else:
+        #     self.random_erasing = None
+
+    def __iter__(self):
+        stream = torch.cuda.Stream()
+        first = True
+
+        for next_input, next_target in self.loader:
+            with torch.cuda.stream(stream):
+                next_input = next_input.cuda(non_blocking=True)
+                next_target = next_target.cuda(non_blocking=True)
+                if self.fp16:
+                    next_input = next_input.half().sub_(self.mean).div_(self.std)
+                else:
+                    next_input = next_input.float().sub_(self.mean).div_(self.std)
+                # if self.random_erasing is not None:
+                #     next_input = self.random_erasing(next_input)
+
+            if not first:
+                yield inputs, target
+            else:
+                first = False
+
+            torch.cuda.current_stream().wait_stream(stream)
+            inputs = next_input
+            target = next_target
+
+        yield inputs, target
+
+    def __len__(self):
+        return len(self.loader)
+
+    @property
+    def sampler(self):
+        return self.loader.sampler
+
+    @property
+    def dataset(self):
+        return self.loader.dataset
+
+def fast_collate(batch):
+    """ A fast collation function optimized for uint8 images (np array or torch) and int64 targets (labels)"""
+    assert isinstance(batch[0], tuple)
+    batch_size = len(batch)
+    if isinstance(batch[0][0], tuple):
+        # This branch 'deinterleaves' and flattens tuples of input tensors into one tensor ordered by position
+        # such that all tuple of position n will end up in a torch.split(tensor, batch_size) in nth position
+        inner_tuple_size = len(batch[0][0])
+        flattened_batch_size = batch_size * inner_tuple_size
+        targets = torch.zeros(flattened_batch_size, dtype=torch.int64)
+        tensor = torch.zeros((flattened_batch_size, *batch[0][0][0].shape), dtype=torch.uint8)
+        for i in range(batch_size):
+            assert len(batch[i][0]) == inner_tuple_size  # all input tensor tuples must be same length
+            for j in range(inner_tuple_size):
+                targets[i + j * batch_size] = batch[i][1]
+                tensor[i + j * batch_size] += torch.from_numpy(batch[i][0][j])
+        return tensor, targets
+    elif isinstance(batch[0][0], np.ndarray):
+        targets = torch.tensor([b[1] for b in batch], dtype=torch.int64)
+        assert len(targets) == batch_size
+        tensor = torch.zeros((batch_size, *batch[0][0].shape), dtype=torch.uint8)
+        for i in range(batch_size):
+            tensor[i] += torch.from_numpy(batch[i][0])
+        return tensor, targets
+    elif isinstance(batch[0][0], torch.Tensor):
+        targets = torch.tensor([b[1] for b in batch], dtype=torch.int64)
+        assert len(targets) == batch_size
+        tensor = torch.zeros((batch_size, *batch[0][0].shape), dtype=torch.uint8)
+        for i in range(batch_size):
+            tensor[i].copy_(batch[i][0])
+        return tensor, targets
+    else:
+        assert False
+
+def get_loaders(root, batch_size, resolution, num_workers=32, val_batch_size=200, prefetch=False):
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
-    train_dataset = ImageFolder(
-        root + "/train",
-        transforms.Compose([
+
+    if not prefetch:
+        transform_train = transforms.Compose([
+                transforms.Resize([resolution, resolution]),
+                transforms.RandomResizedCrop(resolution),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                normalize,
+            ])
+        transform_eval = transforms.Compose([
             transforms.Resize([resolution, resolution]),
-            transforms.RandomResizedCrop(resolution),
-            transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             normalize,
-        ])
+            ])
+    else:
+        transform_train = transforms.Compose([
+                transforms.Resize([resolution, resolution]),
+                transforms.RandomResizedCrop(resolution),
+                transforms.RandomHorizontalFlip(),
+                ToNumpy(),
+            ])
+        transform_eval = transforms.Compose([
+            transforms.Resize([resolution, resolution]),
+            ToNumpy(),
+            ])        
+
+    train_dataset = ImageFolder(
+        root + "/train",
+        transform_train
     )
 
     train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
 
     val_dataset = ImageFolder(
         root + "/val",
-        transforms.Compose([
-            transforms.Resize([resolution, resolution]),
-            transforms.ToTensor(),
-            normalize,
-        ])
+        transform_eval
     )
 
-
+    collate_fn = fast_collate if prefetch else torch.utils.data.dataloader.default_collate
+    
     train_loader = DataLoader(train_dataset,
         batch_size=batch_size, shuffle=(train_sampler is None),
-        num_workers=num_workers, pin_memory=True, sampler=train_sampler
+        num_workers=num_workers, pin_memory=True, sampler=train_sampler,collate_fn=collate_fn
     )
 
     val_loader = DataLoader(val_dataset,
         batch_size=val_batch_size, shuffle=False,
-        num_workers=num_workers, pin_memory=True
+        num_workers=num_workers, pin_memory=True, collate_fn=collate_fn
     )
-    return train_loader, val_loader, train_sampler
+    if prefetch:
+        train_loader = PrefetchLoader(train_loader)
+        val_loader = PrefetchLoader(val_loader)
+
+    return train_loader, val_loader, train_loader.sampler
+
+
 
 
 # def get_loaders(root, batch_size, resolution, device_id, world_size, num_workers=32):
--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@
 Hacked together by / Copyright 2020 Ross Wightman
 """
 import logging
+import numpy as np
 from collections import OrderedDict
 from copy import deepcopy
 
@@ -94,3 +95,13 @@ class AverageMeter(object):
     def __str__(self):
         fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
         return fmtstr.format(**self.__dict__)
+
+
+class ToNumpy:
+
+    def __call__(self, pil_img):
+        np_img = np.array(pil_img, dtype=np.uint8)
+        if np_img.ndim < 3:
+            np_img = np.expand_dims(np_img, axis=-1)
+        np_img = np.rollaxis(np_img, 2)  # HWC to CHW
+        return np_img
