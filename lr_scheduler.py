3120dc4f4af0d4322d6efa14dd4a12e41f60b299
xslidi <591561656@qq.com>
2021-03-24 17:46:41 +0800

    cos scheduler support step decay

--- lr_scheduler.py
+++ lr_scheduler.py
@@ -279,16 +279,15 @@ class CosineAnnealingLR(_LRScheduler):
             # return [self.warmup_lr_init + (base_lr - self.warmup_lr_init) * self.last_epoch / self.warmup_t for base_lr in self.base_lrs] 
         elif self.last_epoch == self.warmup_t:
             return [base_lr for base_lr in self.base_lrs] 
-            
-        # if self.last_epoch == 0:
-        #     return self.base_lrs
+        elif self.last_epoch % self.step_size != 0:
+            return [group['lr'] for group in self.optimizer.param_groups]
         elif (self.last_epoch - 1 - self.T_max) % (2 * self.T_max) == 0:
             return [group['lr'] + (base_lr - self.eta_min) *
                     (1 - math.cos(math.pi / self.T_max)) / 2
                     for base_lr, group in
                     zip(self.base_lrs, self.optimizer.param_groups)]
         return [(1 + math.cos(math.pi * self.last_epoch / self.T_max)) /
-                (1 + math.cos(math.pi * (self.last_epoch - 1) / self.T_max)) *
+                (1 + math.cos(math.pi * (self.last_epoch - self.step_size) / self.T_max)) *
                 (group['lr'] - self.eta_min) + self.eta_min
                 for group in self.optimizer.param_groups]
 
