775c122e392f764b3f4287c83fb96b2fd1ad52d2
xslidi <591561656@qq.com>
2021-03-29 16:55:39 +0800

    rewrite drop-connect like origina repo

--- models/effnet.py
+++ models/effnet.py
@@ -3,11 +3,7 @@ import math
 import torch
 import torch.nn as nn
 
-from models.layers import conv_bn_act
-from models.layers import SamePadConv2d
-from models.layers import Flatten
-from models.layers import SEModule
-from models.layers import DropConnect
+from models.layers import conv_bn_act, SamePadConv2d, Flatten, SEModule, DropConnect
 
 
 class MBConv(nn.Module):
@@ -52,11 +48,11 @@ class MBConv(nn.Module):
 
 
 class MBBlock(nn.Module):
-    def __init__(self, in_, out_, expand, kernel, stride, num_repeat, skip, se_ratio, drop_connect_ratio=0.2):
+    def __init__(self, in_, out_, expand, kernel, stride, num_repeat, skip, se_ratio, drop_connect_ratio=0.2, dc_step=0):
         super().__init__()
         layers = [MBConv(in_, out_, expand, kernel, stride, skip, se_ratio, drop_connect_ratio)]
         for i in range(1, num_repeat):
-            layers.append(MBConv(out_, out_, expand, kernel, 1, skip, se_ratio, drop_connect_ratio))
+            layers.append(MBConv(out_, out_, expand, kernel, 1, skip, se_ratio, drop_connect_ratio + i * dc_step))
         self.layers = nn.Sequential(*layers)
 
     def forward(self, x):
@@ -67,9 +63,14 @@ class EfficientNet(nn.Module):
     def __init__(self, width_coeff, depth_coeff,
                  depth_div=8, min_depth=None,
                  dropout_rate=0.2, drop_connect_rate=0.2,
-                 num_classes=1000):
+                 num_classes=1000, se_r=0.25):
         super().__init__()
         min_depth = min_depth or depth_div
+        channals = [32, 16, 24, 40, 80, 112, 192, 320]
+        expand = [1, 6, 6, 6, 6, 6, 6]
+        k = [3, 3, 5, 3, 5, 5, 3]
+        s = [1, 2, 2, 2, 1, 2, 1]
+        repeat = [1, 2, 2, 3, 3, 4, 1]
         
         def renew_ch(x):
             if not width_coeff:
@@ -84,18 +85,33 @@ class EfficientNet(nn.Module):
         def renew_repeat(x):
             return int(math.ceil(x * depth_coeff))
 
+        def blocks_builter(channals, expand, k, s, repeat):
+            rn_channals = [renew_ch(ch) for ch in channals]
+            rn_repeat = [renew_repeat(rp) for rp in repeat]
+            total_num = sum(rn_repeat)
+            dc_step = drop_connect_rate / total_num
+            blocks = []
+            save_ratio = 0.0
+
+            for i in range(len(rn_repeat)):                
+                blocks.append(MBBlock(rn_channals[i], rn_channals[i+1], expand[i], k[i], s[i], rn_repeat[i], True, se_r, save_ratio, dc_step))
+                save_ratio = save_ratio + rn_repeat[i] * dc_step
+
+            return blocks
+
         self.stem = conv_bn_act(3, renew_ch(32), kernel_size=3, stride=2, bias=False)
         
-        self.blocks = nn.Sequential(
-            #       input channel  output    expand  k  s                   skip  se
-            MBBlock(renew_ch(32), renew_ch(16), 1, 3, 1, renew_repeat(1), True, 0.25, drop_connect_rate),
-            MBBlock(renew_ch(16), renew_ch(24), 6, 3, 2, renew_repeat(2), True, 0.25, drop_connect_rate),
-            MBBlock(renew_ch(24), renew_ch(40), 6, 5, 2, renew_repeat(2), True, 0.25, drop_connect_rate),
-            MBBlock(renew_ch(40), renew_ch(80), 6, 3, 2, renew_repeat(3), True, 0.25, drop_connect_rate),
-            MBBlock(renew_ch(80), renew_ch(112), 6, 5, 1, renew_repeat(3), True, 0.25, drop_connect_rate),
-            MBBlock(renew_ch(112), renew_ch(192), 6, 5, 2, renew_repeat(4), True, 0.25, drop_connect_rate),
-            MBBlock(renew_ch(192), renew_ch(320), 6, 3, 1, renew_repeat(1), True, 0.25, drop_connect_rate)
-        )
+        self.blocks = nn.Sequential(*blocks_builter(channals, expand, k, s, repeat))
+        # self.blocks = nn.Sequential(
+        #     #       input channel  output    expand  k  s                   skip  se
+        #     MBBlock(renew_ch(32), renew_ch(16), 1, 3, 1, renew_repeat(1), True, 0.25, drop_connect_rate),
+        #     MBBlock(renew_ch(16), renew_ch(24), 6, 3, 2, renew_repeat(2), True, 0.25, drop_connect_rate),
+        #     MBBlock(renew_ch(24), renew_ch(40), 6, 5, 2, renew_repeat(2), True, 0.25, drop_connect_rate),
+        #     MBBlock(renew_ch(40), renew_ch(80), 6, 3, 2, renew_repeat(3), True, 0.25, drop_connect_rate),
+        #     MBBlock(renew_ch(80), renew_ch(112), 6, 5, 1, renew_repeat(3), True, 0.25, drop_connect_rate),
+        #     MBBlock(renew_ch(112), renew_ch(192), 6, 5, 2, renew_repeat(4), True, 0.25, drop_connect_rate),
+        #     MBBlock(renew_ch(192), renew_ch(320), 6, 3, 1, renew_repeat(1), True, 0.25, drop_connect_rate)
+        # )
 
         self.head = nn.Sequential(
             *conv_bn_act(renew_ch(320), renew_ch(1280), kernel_size=1, bias=False),
@@ -125,10 +141,11 @@ class EfficientNet(nn.Module):
 if __name__ == "__main__":
     print("Efficient B0 Summary")
     net = EfficientNet(1, 1)
-    for k, v in net.named_parameters():
-        print(k)
-    print("----------------------------------------")
-    for k, ema_v in net.state_dict().items():
-        print(k)
+    print(net)
+    # for k, v in net.named_parameters():
+    #     print(k)
+    # print("----------------------------------------")
+    # for k, ema_v in net.state_dict().items():
+    #     print(k)
     # from torchsummary import summary
     # summary(net.cuda(), (3, 224, 224))
